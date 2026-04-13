39c297e02686a5b4ceed50002c6059f12f93db77
kazurayam <kazuaki.urayama@gmail.com>
2026-04-13 20:47:00 +0900

    small chane

--- Keywords/com/kazurayam/ks/BrowserMobProxyManager.groovy
+++ Keywords/com/kazurayam/ks/BrowserMobProxyManager.groovy
@@ -4,13 +4,9 @@ import com.kms.katalon.core.annotation.Keyword
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
 public class BrowserMobProxyManager {
-	
-	public static void main(String[] args) {
-		greeting()
-	}
-	
+
 	@Keyword
 	public static void greeting() {
-		WebUI.comment("Hello, i am " + BrowserMobProxyManager.class.getName())
+		WebUI.comment("Hello, i am " + BrowserMobProxyManager.class.getSimpleName())
 	}
 }
